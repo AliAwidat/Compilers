6deeb64ed54cf7683d2031668ab271539b9d153e
AliAwidat <36275593+AliAwidat@users.noreply.github.com>
2020-04-12 20:49:13 +0300

    Added declaration case+print table
    Added declaration case+print table

--- compiler_files/CodeGenerator.c
+++ compiler_files/CodeGenerator.c
@@ -14,12 +14,12 @@ typedef struct symbol_table {
 	 * table_adress_counter is for tracking last variable adress, we start with adress 5
 	 * table_content is a pointer pointing to the head of the variables linked list */
 	int table_size;
-	int table_adress_counter;
 	Variable *table_content;
 } Symbol_table;
 
 Symbol_table *table;
 int label_counter=1;
+int table_adress_counter=5;
 /*
 *	You need to build a data structure for the symbol table
 *	I recommend to use linked list.
@@ -85,8 +85,7 @@ void insertVar(Variable *head,Variable *new_var){
 	return;
 }
 
-Variable * createVar(char *var_name,char *var_type,int var_size,int var_adress,Variable *var_next,Variable *var_prev){
-
+Variable * createVar(char *var_name,char *var_type,int var_size,Variable *var_next,Variable *var_prev){
 	Variable *new_var;
 	new_var=(Variable *)malloc(sizeof(Variable));
 	if(new_var == NULL){
@@ -95,7 +94,7 @@ Variable * createVar(char *var_name,char *var_type,int var_size,int var_adress,V
 	}
 	new_var->var_name=var_name;
 	new_var->var_type=var_type;
-	new_var->var_adress=var_adress;
+	new_var->var_adress=table_adress_counter++;
 	new_var->var_size=var_size;
 	new_var->var_next=var_next;
 	new_var->var_prev=var_prev;
@@ -127,6 +126,7 @@ int removeVar(Variable *var_to_remove){
 int  code_recur(treenode *root)
 {
 	int test ;
+	char *tmp_name,*tmp_type;
 	if_node  *ifn;
 	for_node *forn;
 	leafnode *leaf;
@@ -380,9 +380,25 @@ int  code_recur(treenode *root)
 					break;
 					
 				case TN_DECL:
-					/* structs declaration - for HW2 */
-					code_recur(root->lnode);
-					code_recur(root->rnode);
+					leaf = (leafnode*)root->rnode;
+					tmp_name=leaf->data.sval->str;
+					leaf = (leafnode*)root->lnode->lnode;
+					switch(leaf->hdr.tok){
+						case INT:
+							tmp_type="int";
+							break;
+						case FLOAT:
+							tmp_type="float";
+							break;
+						case DOUBLE:
+							tmp_type="double";
+							break;
+						default:
+							tmp_type=NULL;
+							break;
+					}
+					Variable *new_var=createVar(tmp_name,tmp_type,1,NULL,NULL);
+					insertVar(table->table_content,new_var);
 					break;
 
 				case TN_DECL_LIST:
@@ -798,7 +814,12 @@ int  code_recur(treenode *root)
 void print_symbol_table(treenode *root) {
 	printf("---------------------------------------\n");
 	printf("Showing the Symbol Table:\n");
-	/*
-	*	add your code here
-	*/
+	printf("---------------------------------------\n");
+	Variable *tmp=table->table_content;
+	while(tmp){
+		printf("name: %s| type: %s| adress: %d| size: %d|\n",tmp->var_name,tmp->var_type,tmp->var_adress,tmp->var_size);
+	}
+	printf("---------------------------------------\n");
+
+
 }
